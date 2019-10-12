8d9f5099a92f1ddfe7a1e0fedbb09f1f2903f3b4
Mishan Madhupa <mishancool2016@gmail.com>
2019-10-12 23:35:44 +0530

    Post router as authenticate

--- Server/routes/index.router.js
+++ Server/routes/index.router.js
@@ -5,6 +5,7 @@
  const ctrlUser = require('../controllers/user.controller');
 
  router.post('/register', ctrlUser.register); //('uri', function wich can handal, user sing up request in the client side)
+ router.post('/authenticate', ctrlUser.authenticate);
 
  //can configer routing middleware inside this application
  module.exports = router;
