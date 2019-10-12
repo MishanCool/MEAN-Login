21b787fce22bdc87d6f9c604bbd93cc681e2fb18
Mishan Madhupa <mishancool2016@gmail.com>
2019-10-12 23:31:31 +0530

    Add post route function to send credential email and password for authentication

--- Server/controllers/user.controller.js
+++ Server/controllers/user.controller.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const passport = require('passport');
 
 const User = mongoose.model('User');
 
@@ -36,4 +37,25 @@ module.exports.register = (req, res, next) =>{
         }
     });
 
+}
+
+
+module.exports.authenticate = (req, res, next) => {
+    //call for passport authentication
+    passport.authenticate('local', (err,user,info) => {
+        //error in passport middleware
+        if(err){
+            return res.status(400).json(err);
+        }
+
+        //register user
+        else if(user){
+            return res.status(200).json({ "token": user.genarateJwt() });
+        }
+
+        //unknown user or wrong password
+        else{
+            return res.status(404).json(info);
+        }
+    })(req,res);
 }
