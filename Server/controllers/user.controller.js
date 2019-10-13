c1c9f787056b57eaa24b54dcaa14b0cf83a44a60
Mishan Madhupa <mishancool2016@gmail.com>
2019-10-13 23:29:12 +0530

    Create userProfile function

--- Server/controllers/user.controller.js
+++ Server/controllers/user.controller.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const passport = require('passport');
+const _ = require('lodash');
 
 const User = mongoose.model('User');
 
@@ -50,7 +51,7 @@ module.exports.authenticate = (req, res, next) => {
 
         //register user
         else if(user){
-            return res.status(200).json({ "token": user.genarateJwt() });
+            return res.status(200).json({ "token": user.generateJwt() });
         }
 
         //unknown user or wrong password
@@ -58,4 +59,15 @@ module.exports.authenticate = (req, res, next) => {
             return res.status(404).json(info);
         }
     })(req,res);
+}
+
+module.exports.userProfile = (req, res, next) => {
+    User.findOne({ _id: req._id }, (err, user) => {
+        if(!user){
+            return res.status(404).json({ status: false, message: 'User recodes not found' });
+        }
+        else{
+            return res.status(200).json({ status: true, user: _.pick(user,['username','email']) });
+        }
+    });
 }
