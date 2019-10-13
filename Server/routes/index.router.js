7cada8c60b4c8f670cc1714845e9279509bd9163
Mishan Madhupa <mishancool2016@gmail.com>
2019-10-13 23:30:43 +0530

    Create private route using jwt

--- Server/routes/index.router.js
+++ Server/routes/index.router.js
@@ -4,8 +4,14 @@
  //ctrl mean controller
  const ctrlUser = require('../controllers/user.controller');
 
+ const jwtHelper = require('../config/jwtHelper');
+
+ //post router
  router.post('/register', ctrlUser.register); //('uri', function wich can handal, user sing up request in the client side)
  router.post('/authenticate', ctrlUser.authenticate);
+
+ //get router
+ router.get('/userProfile', jwtHelper.verifyJwtToken, ctrlUser.userProfile); // call function 1 =>jwtHelper.verifyJwtToken function befor ctrlUser function  to verify jwt token wich is curently atach to this request
 
  //can configer routing middleware inside this application
  module.exports = router;
