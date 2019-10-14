c78a4d7a5314d29978a27d0196e68e32b93e5c81
Mishan Madhupa <mishancool2016@gmail.com>
2019-10-14 22:38:32 +0530

    ## Client side user and sign-up components

---
+++ client/angularWeb/src/app/app.module.ts
@@ -0,0 +1,33 @@
+// building imports
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
+// components imports
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserComponent } from './components/user/user.component';
+import { SignUpComponent } from './components/user/sign-up/sign-up.component';
+
+// routes
+import { appRoutes } from './routes';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserComponent,
+    SignUpComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
