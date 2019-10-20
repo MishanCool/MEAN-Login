353e328d5c1cebe2b0a512c85f6fd413803c8ffa
Mishan Madhupa <mishancool2016@gmail.com>
2019-10-20 23:52:49 +0530

    If give path as userprofile in http bar, redirect to the login page

--- client/angularWeb/src/app/auth/auth.guard.ts
+++ client/angularWeb/src/app/auth/auth.guard.ts
@@ -1,24 +1,42 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, CanActivateChild, CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+// import { CanActivate, CanActivateChild, CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
+
+import { UserService } from './../shared/user.service';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate, CanActivateChild, CanLoad {
+// export class AuthGuard implements CanActivate, CanActivateChild, CanLoad {
+export class AuthGuard implements CanActivate {
+
+  constructor(
+    private userService: UserService,
+    private router: Router
+  ) {}
+
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    // state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    state: RouterStateSnapshot): boolean {
+
+      if (!this.userService.isLoggedIn()) {
+        this.router.navigateByUrl('/login');
+        this.userService.deleteToken();
+        return false;
+      }
+      return true;
   }
-  canActivateChild(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
-  }
-  canLoad(
-    route: Route,
-    segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
-  }
+  // canActivateChild(
+  //   next: ActivatedRouteSnapshot,
+  //   state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  //   return true;
+  // }
+  // canLoad(
+  //   route: Route,
+  //   segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
+  //   return true;
+  // }
 }
