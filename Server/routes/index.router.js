2f0fe1c3970102a090232165fc86746ee6669378
Mishan Madhupa <mishancool2016@gmail.com>
2019-10-22 06:56:47 +0530

    ## Complete registration and login form with viewing user profile

--- Server/routes/index.router.js
+++ Server/routes/index.router.js
@@ -12,6 +12,7 @@
 
  //get router
  router.get('/userProfile', jwtHelper.verifyJwtToken, ctrlUser.userProfile); // call function 1 =>jwtHelper.verifyJwtToken function befor ctrlUser function  to verify jwt token wich is curently atach to this request
+// first verify jwt token and then give access userprofile
 
  //can configer routing middleware inside this application
  module.exports = router;
